a395db7fb71a24fc0224e17f75e4c8c82037a482
Theo Belaire <theo.belaire@gmail.com>
2015-04-15 22:08:13 -0400

    Explained logging more

--- enable_logging.sh
+++ enable_logging.sh
@@ -1,2 +1,18 @@
+# Logging levels
+# error
+# warning
+# info
+# debug
+# trace
+
 
 export RUST_LOG=rust_blob=info,rust_blob::draw=debug,draw=info
+
+# If you want to see everything going on,
+# export RUST_LOG=trace
+
+# To silence all the logging messages:
+# export RUST_LOG=
+
+
+# You can turn on different levels of logging for different modules
